77ee796f4c9513c35cfa0dc01c4b1ba12a0ba242
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2022-02-16 07:25:25 -0800

    Edited 0-binary_tree_node.c file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,12 +7,12 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-  binary_tree_t *btn = (binary_tree_t*)malloc(sizeof(binary_tree_t));
-  if (btn == NULL)
-    return (NULL);
-  btn->n = value;
-  btn->parent = parent;
-  btn->left = NULL;
-  btn->right = NULL;
-  return (btn);
+binary_tree_t *btn = (binary_tree_t*)malloc(sizeof(binary_tree_t));
+if (btn == NULL)
+return (NULL);
+btn->n = value;
+btn->parent = parent;
+btn->left = NULL;
+btn->right = NULL;
+return (btn);
 }
